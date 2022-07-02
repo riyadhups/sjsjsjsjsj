32712f5124d5aa6ab345e22b3698df51f0928370
riyadhups <101044168+riyadhups@users.noreply.github.com>
2022-07-02 20:40:03 +0400

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Rahid_Asistan") 
+BOT_NAME = getenv("BOT_NAME", "KafkazAsistan") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Rahid_Music_Bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "KafkazMusicbot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
